47f131c119e24507ff8f6587af36d37d305299c9
SimonBao <Simonbao.sb@gmail.com>
2021-09-13 19:48:02 +0100

    Implement api calls to change data store

--- src/App.js
+++ src/App.js
@@ -36,6 +36,12 @@ function App() {
     getTasks();
   }, []);
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
+
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks');
     const data = await res.json();
@@ -46,18 +52,35 @@ function App() {
     setShowAddTask(!showAddTask);
   }
 
-  const addTask = (task) => {
-    const id = tasks.length + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', { 
+      method: 'POST',
+      headers: { 'Content-type': 'application/json'},
+      body: JSON.stringify(task)
+    });
+    const data = await res.json();
+    setTasks([...tasks,data]);
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' });
     setTasks(tasks.filter((task) => task.id !== id ));
   }
 
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task));
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, { 
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    });
+
+    const data = await res.json();
+    setTasks(tasks.map((task) => task.id === id ? data : task));
   }
 
   return (
