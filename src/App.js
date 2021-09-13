4c311de57d4e48640d9d32686be10de5c4a30cd5
SimonBao <Simonbao.sb@gmail.com>
2021-09-13 17:00:33 +0100

    Remove task from task tracker when x button is clicked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import { useState} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Driving Lesson',
+      day: 'March 19th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Dentist Appointment',
+      day: 'November 7th at 5:30pm',
+      reminder: true,
+    }
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,10 @@
-import { useState} from 'react';
 import Task from './Task';
 
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Driving Lesson',
-      day: 'March 19th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Dentist Appointment',
-      day: 'November 7th at 5:30pm',
-      reminder: true,
-    }
-  ])
-
+const Tasks = ({tasks, onDelete}) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} />
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   )
