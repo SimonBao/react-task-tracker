d18621cfb467c0bebaa7238635cbb21ca91e03ca
SimonBao <Simonbao.sb@gmail.com>
2021-09-13 21:00:12 +0100

    Change checkbox to mirror actual value

--- src/components/Add.js
+++ src/components/Add.js
@@ -31,7 +31,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       <div className="form-control form-control-check">
         <label>Set Reminder</label>
-        <input type="checkbox" value={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
+        <input type="checkbox" value={reminder} checked={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
       </div>
       <input type="submit" value="Save Task" className="btn btn-block" />
     </form>
